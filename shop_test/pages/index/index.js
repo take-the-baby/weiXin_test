bc613ff4be8655e90056c9431b6ad4d16e8f5486
zhanlidai <2668479189@qq.com>
2020-06-21 18:47:05 +0800

    完成基本项目搭建并完成首页模块

--- shop_test/pages/index/index.js
+++ shop_test/pages/index/index.js
@@ -1,54 +1,47 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
+//index.jspa
+import { request } from '../../request/index.js';
 Page({
+  /**
+   * 页面的初始数据
+   */
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    swiperList: [], // 轮播图数据
+    catesList: [], // 分类导航数据
+    floorData: [], // 楼层视图数据
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  onLoad: function(options) {
+    this.getSwiperData();
+    this.getCateData();
+    this.getFloorData();
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
+  // 获取轮播图数据
+  getSwiperData() {
+    request({url:'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata'}).then(res =>{
+      if(res.data && res.data.meta.status == 200) {
         this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+        swiperList: res.data.message
         })
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    }) 
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+  // 获取分类导航数据
+  getCateData() {
+    request({url:'https://api-hmugo-web.itheima.net/api/public/v1/home/catitems'}).then(res =>{
+      if(res.data && res.data.meta.status == 200) {
+        this.setData({
+          catesList: res.data.message
+        })
+      }
+    }) 
+  },
+  // 获取楼层视图的数据
+  getFloorData() {
+    request({url:'https://api-hmugo-web.itheima.net/api/public/v1/home/floordata'}).then(res =>{
+      if(res.data && res.data.meta.status == 200) {
+        this.setData({
+          floorData: res.data.message
+        })
+      }
+    }) 
   }
 })
