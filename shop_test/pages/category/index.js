ccb1e73d57bd1a7d6630fe86a243268467355ee8
zhanlidai <2668479189@qq.com>
2020-06-21 21:46:06 +0800

    分类页面功能布局基本完成

--- shop_test/pages/category/index.js
+++ shop_test/pages/category/index.js
@@ -1,66 +1,66 @@
 // pages/category/index.js
+import { request } from '../../request/index.js'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    leftListData: [],// 左边列表数据
+    rightListData: [], // 右边列表数据
+    cates:[], // 接口返回数据
+    activeIndex: 0, 
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // 缓存接口数据，如果本地有取本地数据
+    const cates = wx.getStorageSync('cates')
+    if(!cates) {
+      this.getCatesData()
+    } else {
+      // 如果超过5分钟，重新发送请求
+      if(Date.now() - cates.time > 1000*60*5) {
+        this.getCatesData()
+      } else {
+        const resData = cates.data;
+        const leftListData = resData.map(item =>{return item.cat_name});
+        const rightListData = resData[0].children;
+        this.setData({
+          cates: resData,
+          leftListData: leftListData,
+          rightListData: rightListData
+        })
+      }
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 获取分类数据
+  getCatesData() {
+    request({'url':'https://api-hmugo-web.itheima.net/api/public/v1/categories'}).then(res =>{
+      if(res.data && res.data.meta.status == 200) {
+        wx.setStorageSync('cates', {
+          time: Date.now(),
+          data:  res.data.message
+        })
+        const resData = res.data.message;
+        const leftListData = resData.map(item =>{return item.cat_name});
+        const rightListData = resData[0].children;
+        this.setData({
+          cates: resData,
+          leftListData: leftListData,
+          rightListData: rightListData
+        })
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 左侧列表点击事件
+  leftListClick(e) {
+    const {index} = e.currentTarget.dataset;
+    const rightListData = this.data.cates[index].children;
+    this.setData({
+      activeIndex: index,
+      rightListData: rightListData
+    })
   }
 })
