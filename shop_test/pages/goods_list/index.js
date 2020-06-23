91e7c77dd5d50c46ec0f0ce08cd74f148d96607a
zhanlidai <2668479189@qq.com>
2020-06-23 13:18:10 +0800

    完成对商品列表的动态渲染

--- shop_test/pages/goods_list/index.js
+++ shop_test/pages/goods_list/index.js
@@ -1,4 +1,5 @@
 // pages/goods_list/index.js
+import { request } from '../../request/index.js'
 Page({
 
   /**
@@ -22,13 +23,29 @@ Page({
         isActive: false,
       }
     ],
+    querySendData:{ // 接口发送参数
+      query:'', // 关键字
+      cid: '', // 分类id
+      pagenum: 1, // 页码
+      pagesize: 10, // 页容量
+    },
+    goodsListData: [], // 商品列表数据
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    const tempData = {
+      query:'', // 关键字
+      cid: options.cid, // 分类id
+      pagenum: 1, // 页码
+      pagesize: 10, // 页容量
+    }
+    this.setData({
+      querySendData: tempData
+    })
+    this.getGoodsListData()
   },
   // tabs组件的点击事件
   tabsClick(e) {
@@ -41,5 +58,14 @@ Page({
       tabsData: tempTabsData
     })
   },
-
+  // 获取商品列表数据
+  async getGoodsListData() {
+    const res = await request({url:'/goods/search',data:this.data.querySendData});
+    console.log(res)
+    if(res.data && res.data.meta.status == 200) {
+      this.setData({
+        goodsListData: res.data.message.goods
+      })
+    } 
+  }
 })
